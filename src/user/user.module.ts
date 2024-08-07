9ed8b4e01417b9a6df511b94669d069a6fd03828
Richard Dewan <richarddewan@hotmail.com>
2024-08-07 21:31:23 +0700

    fix firebase module import

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,11 @@ import { UserController } from './user.controller';
 import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
 import RepositoryError from 'src/common/errors/repository-error';
+import { FirebaseModule } from 'src/firebase/firebase.module';
 
 @Module({
   controllers: [UserController],
   providers: [UserRepository, UserService, RepositoryError],
+  imports: [FirebaseModule],
 })
 export class UserModule {}
