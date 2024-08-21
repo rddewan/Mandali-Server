43c3bf70ea7b02d6002bc4092caabd588a870896
Richard Dewan <richarddewan@hotmail.com>
2024-08-21 17:10:39 +0700

    update me response with photo url

--- src/me/me.service.ts
+++ src/me/me.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { MeRepository } from './me.repository';
 import { UpdateUserDto } from './dtos';
+import { S3Service } from 'src/aws/s3/s3.service';
 
 @Injectable()
 export class MeService {
-  constructor(private readonly meRepository: MeRepository) {}
+  constructor(
+    private readonly meRepository: MeRepository,
+    private readonly s3Service: S3Service,
+  ) {}
 
   async getUserRoles(userId: number) {
     return await this.meRepository.getUserRoles(userId);
@@ -12,11 +16,16 @@ export class MeService {
 
   async me(id: number) {
     const user = await this.meRepository.me(id);
+    // Get the signed URL for the user's photo if it exists
+    const photo = user.photo
+      ? await this.s3Service.getSignedUrl(user.photo, 3600)
+      : null;
     return {
       id: user.id,
       name: user.name,
       email: user.email,
       phoneNumber: user.phoneNumber,
+      photo,
       role: user.roles.map((role) => role.role),
       church: {
         id: user.church.id,
